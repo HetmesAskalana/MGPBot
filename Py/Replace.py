27f3616dd9fd5d18cfacddd72d5a271f2d122f36
HetmesAskalana <60427521+HetmesAskalana@users.noreply.github.com>
2022-06-25 17:02:24 +0800

    Update Replace.py

--- Py/Replace.py
+++ Py/Replace.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+from distutils import filelist
+from distutils.command.upload import upload
+from pickle import FALSE
 import requests;
 import re;
 import os;
@@ -7,11 +10,28 @@ import time;
 import json;
 
 
+
 def get_text(pageid, section = "", spaceid = 'ZH'):
     workspace = WORKSPACE[spaceid]
     PARAMS = {
         'action': "parse",
         'pageid': pageid,
+        'prop': 'wikitext',
+        'section': section,
+        'format': "json"
+    }
+    if section == "":
+        PARAMS.pop('section')
+    res = workspace['SESSION'].get(url=workspace['URL'], params=PARAMS)
+    data = res.json()
+    #print(data)
+    return data['parse']['wikitext']['*']
+
+def get_text_as_title(title, section = "", spaceid = 'ZH'):
+    workspace = WORKSPACE[spaceid]
+    PARAMS = {
+        'action': "parse",
+        'page': title,
         'prop': 'wikitext',
         'section': section,
         'format': "json"
@@ -123,46 +143,47 @@ def page_replace(pageid, find, replace, spaceid = 'ZH'):
     wikitext = wikitext.replace(find, replace)
     page_edit(pageid, wikitext, True, "文本替换 - 替换『" + find + "』为『" + replace + "』", spaceid)
 
+def page_replace_as_title(title, find, replace, spaceid = 'ZH'):
+    wikitext = get_text_as_title(title, "", spaceid)
+    wikitext = wikitext.replace(find, replace)
+    editAsTitle(title, wikitext, True, "文本替换 - 替换『" + find + "』为『" + replace + "』", spaceid)
 
 def page_regsub(pageid, find, replace, spaceid = 'ZH'):
     wikitext = get_text(pageid, "", spaceid)
     wikitext = re.sub(find, replace, wikitext)
     page_edit(pageid, wikitext, True, "文本替换 - 替换『" + find + "』为『" + replace + "』", spaceid)
 
-def upload_file():
+def upload_file(igwarn = False):
     workspace = WORKSPACE['COM'];
     fileList = os.listdir("");
     list1=[];
-    for name in fileList:
-        if os.path.splitext(name)[1] != ".jpg":
-            continue;
-        list1.append(os.path.splitext(name)[0]+os.path.splitext(name)[1]);
     for fileName in fileList:
         PARAMS = {
             "action": "upload",
             "format": "json",
             "smaxage": "0",
             "filename": fileName,
-            "text": "[[Category:蔚蓝档案CG]]",
+            "text": "[[Category:秤亚津子语音]]",
             "watchlist": "nochange",
             "token": workspace['csrftoken'],
-            "tags": "Bot"
+            "tags": "Bot",
+            "ignorewarnings": igwarn
         };
-        FILE = {'file':(fileName, open("D:/DATA/蔚蓝档案/解包/语音/php/test1/UIs/03_Scenario/01_Background/"+fileName, 'rb'), 'multipart/form-data')};
+        FILE = {'file':(fileName, open(""+fileName, 'rb'), 'multipart/form-data')};
         R = workspace['SESSION'].post(url=WORKSPACE['COM']['URL'], files=FILE, data=PARAMS);
         DATA = R.json();
-        if DATA['upload']['result'] == 'Success':
+        print(DATA);
+        '''if DATA['upload']['result'] == 'Success':
             print(fileName + "已成功上传");
         else:
             print(fileName + "上传失败");
-            print(DATA);
+            print(DATA);'''
     print("exit");
-#该函数无效，需要拎出来用
 
 def delete_s(pages, reason, spaceid = "ZH"):
     for page in pages:
         editAsTitle(page, "<noinclude>{{即将删除|1="+ reason +"|user=HetmesAskalana}}</noinclude>", False, "挂删："+ reason, spaceid);
-        time.sleep(20)
+        time.sleep(8)
 
 def readLine(path):
     file = open(path, 'r', encoding='utf-8');
@@ -174,26 +195,51 @@ def readLine(path):
         list1.append(tmp);
     return list1;
 
+def movePage(src, to, reason, noredirect = True, sub = False, talk = True, spaceid = "ZH"):
+    workspace = WORKSPACE[spaceid];
+    PARAMS = {
+        "action": "move",
+        "format": "json",
+        "from": src,
+        "to": to,
+        "reason": reason,
+        "tags": "Bot",
+        "token": workspace['csrftoken']
+    };
+    if noredirect:
+        PARAMS["noredirect"] = True;
+    if sub:
+        PARAMS["movesubpages"] = True;
+    if talk:
+        PARAMS["movetalk"] = True;
+    res = workspace['SESSION'].post(workspace['URL'], data=PARAMS);
+    data = res.json();
+    print(data);
+    return data;
+
 
 
 def main():
     login('COM');
-    pagelist = readLine("");
-    delete_s(pagelist, "命名错误的文件及其移动后残留的重定向", "COM");
 
-    '''PARAMS = {
+    '''pagelist = readLine("");
+    delete_s(pagelist, "移动导致的重复上传", "COM");'''
+
+    PARAMS = {
         "action": "query",
         "format": "json",
         "list": "search",
-        "srsearch": "奥利斯分校"
+        "srsearch": "BA_V_Atsuko",
+        "srnamespace": "6",
+        "srlimit": "max"
     }
-    res = WORKSPACE['ZH']['SESSION'].get(url=WORKSPACE['ZH']['URL'], params=PARAMS);
+    res = WORKSPACE['COM']['SESSION'].get(url=WORKSPACE['COM']['URL'], params=PARAMS);
     data = res.json();
     members = data['query']['search'];
     print(members);
 
-    for page in members:
-        page_replace(page['pageid'], "奥利斯分校", "阿里乌斯分校", 'ZH')'''
+    '''for page in members:
+        page_replace(page['pageid'], "Category:蔚蓝档案语音", "Category:秤亚津子语音", 'ZH')'''
         
 
 
